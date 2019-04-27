e3866f3071e94c4c192dccc2bd9f6b22f7b4f2c9
Robert Worgul <robert.worgul@scitotec.de>
2019-04-27 23:38:01 +0200

    Fix test

--- src/test/java/de/delbertooo/payoff/apiserver/purchases/purchasingapi/PurchasesServiceTest.java
+++ src/test/java/de/delbertooo/payoff/apiserver/purchases/purchasingapi/PurchasesServiceTest.java
@@ -8,27 +8,30 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class PurchasesServiceTest {
 
     private PurchasesService purchasesService;
     private PurchaseRepository purchaseRepository;
+    private PurchaseToPurchaseCreateTransformer purchaseToPurchaseCreateTransformer;
 
     @Before
     public void setUp() {
         purchaseRepository = mock(PurchaseRepository.class);
+        purchaseToPurchaseCreateTransformer = mock(PurchaseToPurchaseCreateTransformer.class);
         purchasesService = new PurchasesService(
                 purchaseRepository,
                 mock(PurchaseToPurchaseViewTransformer.class),
                 mock(PurchasesCountToPurchaseYearTransformer.class),
-                mock(PurchaseToPurchaseCreateTransformer.class)
+                purchaseToPurchaseCreateTransformer
         );
     }
 
@@ -42,6 +45,11 @@ public class PurchasesServiceTest {
         val argument = ArgumentCaptor.forClass(Purchase.class);
         val create = new PurchasesService.PurchaseCreate();
         GlobalClock.freeze(LocalDateTime.parse("2018-03-13T12:00"));
+        purchaseToPurchaseCreateTransformer.updateWith(Mockito.any(), Mockito.any());
+        doAnswer(i -> {
+            ((Purchase) i.getArgument(0)).setPrice(new BigDecimal("13.37"));
+            return null;
+        }).when(purchaseToPurchaseCreateTransformer).updateWith(any(), any());
 
         purchasesService.createPurchase(create);
 
