45e54d5725d57f0abe96c9a50aae70732ed75e23
Robert Worgul <robert.worgul@scitotec.de>
2019-04-27 20:36:35 +0200

    Improve queries with fetch joins

--- src/main/java/de/delbertooo/payoff/apiserver/purchases/PurchaseRepository.java
+++ src/main/java/de/delbertooo/payoff/apiserver/purchases/PurchaseRepository.java
@@ -2,23 +2,14 @@ package de.delbertooo.payoff.apiserver.purchases;
 
 import de.delbertooo.payoff.apiserver.common.jpa.FlushRepository;
 import de.delbertooo.payoff.apiserver.common.jpa.LockRepository;
-import lombok.val;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Month;
 import java.util.List;
 import java.util.Optional;
 
 public interface PurchaseRepository extends CrudRepository<Purchase, Long>, FlushRepository, LockRepository {
-
-    @Deprecated
-    @Transactional(Transactional.TxType.SUPPORTS)
-    List<Purchase> findByPurchasedAtBetween(LocalDateTime start, LocalDateTime end);
 
     @Transactional(Transactional.TxType.SUPPORTS)
     @Query("SELECT NEW de.delbertooo.payoff.apiserver.purchases.PurchasesCount(p.purchasedYear, count(p.purchasedYear)) " +
@@ -26,12 +17,8 @@ public interface PurchaseRepository extends CrudRepository<Purchase, Long>, Flus
     List<PurchasesCount> findYearlyCounts();
 
     @Transactional(Transactional.TxType.SUPPORTS)
-    default List<Purchase> findByYear(int year) {
-        val start = LocalDate.ofYearDay(year, 1).atStartOfDay();
-        val end = LocalDate.of(year, Month.DECEMBER, 31).atTime(LocalTime.MAX);
-        return findByPurchasedAtBetween(start, end);
-    }
-
+    @Query("SELECT p FROM Purchase p JOIN FETCH p.participants JOIN FETCH p.purchaser WHERE p.purchasedYear = :year")
+    List<Purchase> findByYear(int year);
 
     @Deprecated
     @Transactional(Transactional.TxType.SUPPORTS)
