4edfb2f53ba084fae7d517c5b75800860b1368cb
Robert Worgul <robert.worgul@scitotec.de>
2019-04-28 15:48:35 +0200

    Make dev fixtures runner opt-in

--- src/main/java/de/delbertooo/payoff/apiserver/CreateUsersRunner.java
+++ src/main/java/de/delbertooo/payoff/apiserver/CreateUsersRunner.java
@@ -6,22 +6,31 @@ import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
 import java.util.UUID;
 import java.util.stream.Stream;
 
+@Profile("dev")
 @Component
-public class UsersRunner implements ApplicationRunner {
+public class CreateUsersRunner implements ApplicationRunner {
+    private UserRepository userRepository;
+
     @Autowired
-    UserRepository userRepository;
+    public CreateUsersRunner(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     @Transactional
     public void run(ApplicationArguments args) throws Exception {
-        val delbertooo = new User().setName("delbertooo").setEmail("del@ber.de");
-        val foo = new User().setName("foo").setEmail("del@asdf.de");
+        if (!args.containsOption("create-users")) {
+            return;
+        }
+        val delbertooo = new User().setName("delbertooo").setEmail("del@payoff");
+        val foo = new User().setName("foo").setEmail("foo@payoff");
         val masuuk = new User().setName("masuuk");
 
         Stream.of(delbertooo, masuuk, foo)
